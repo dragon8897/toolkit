7366591a64f055e664cf8416deaad07af73df883
dragon <dragon@fire.com>
2022-06-13 14:39:57 +0800

    feat(core): 添加 if 接口

--- toolkit.go
+++ toolkit.go
@@ -5,3 +5,11 @@ import "fmt"
 func Version() {
 	fmt.Println("toolkit version: 0.0.1 ")
 }
+
+// If 三元表达式
+func If(condition bool, trueVal, falseVal interface{}) interface{} {
+	if condition {
+		return trueVal
+	}
+	return falseVal
+}
--- toolkit_test.go
+++ toolkit_test.go
@@ -5,3 +5,12 @@ import "testing"
 func TestVersion(t *testing.T) {
 	Version()
 }
+
+func TestIf(t *testing.T) {
+	if If(true, 1, 2).(int) != 1 {
+		t.Error("wrong ans")
+	}
+	if If(false, 1, 2).(int) != 2 {
+		t.Error("wrong ans")
+	}
+}
